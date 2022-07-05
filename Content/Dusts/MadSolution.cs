b481da0ddce6b3206f22fcccb2e6c43fe1994564
jv.naka <joaovtnakamura@gmail.com>
2022-07-05 15:16:03 -0300

    Removes untracked content

--- Content/Dusts/MadSolution.cs
+++
@@ -1,9 +0,0 @@
-﻿using Terraria.ModLoader;
-
-namespace FFMod.Content.Dusts
-{
-    public sealed class MadSolution : ModDust
-    {
-        public override void SetStaticDefaults() => UpdateType = 110;
-    }
-}
